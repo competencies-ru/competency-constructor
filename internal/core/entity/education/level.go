71f1eac23a22be83ad7fdf1ebdc0f9f7648e3978
Max Tsilyurik <tsilyurik.maxim@gmail.com>
2023-04-03 18:18:22 +0300

    add level test

--- internal/core/entity/education/level.go
+++ internal/core/entity/education/level.go
@@ -7,7 +7,6 @@ import (
 var (
 	ErrLevelIDIsEmpty    = errors.New("level: id is empty")
 	ErrLevelTitleIsEmpty = errors.New("level: title is empty")
-	ErrLevelUgsnNotFound = errors.New("level: ugsn not found")
 )
 
 func IsInvalidLevelParametersError(err error) bool {
@@ -20,8 +19,6 @@ type (
 	Level struct {
 		id    string
 		title string
-
-		ugsn map[string]*Ugsn
 	}
 
 	LevelParam struct {
@@ -42,7 +39,6 @@ func NewLevel(param LevelParam) (*Level, error) {
 	return &Level{
 		id:    param.ID,
 		title: param.Title,
-		ugsn:  make(map[string]*Ugsn),
 	}, nil
 }
 
@@ -52,42 +48,4 @@ func (l *Level) ID() string {
 
 func (l *Level) Title() string {
 	return l.title
-}
-
-func (l *Level) Ugsn(code string) (*Ugsn, error) {
-	value, ok := l.ugsn[code]
-
-	if !ok {
-		return nil, errors.Wrapf(
-			ErrLevelUgsnNotFound,
-			"get ugsn by code: %s", code)
-	}
-
-	return value, nil
-}
-
-func (l *Level) AddUgsn(param UgsnParams) error {
-	if _, ok := l.ugsn[param.Code]; ok {
-		return nil
-	}
-
-	ugsn, err := NewUgsn(param)
-	if err != nil {
-		return err
-	}
-
-	l.ugsn[ugsn.Code()] = ugsn
-
-	return nil
-}
-
-func (l *Level) AllUgsn() []*Ugsn {
-	result := make([]*Ugsn, 0, len(l.ugsn))
-
-	for _, ugsn := range l.ugsn {
-		tmp := ugsn
-		result = append(result, tmp)
-	}
-
-	return result
 }
---
+++ internal/core/entity/education/level_test.go
@@ -0,0 +1,76 @@
+package education_test
+
+import (
+	"github.com/competencies-ru/competency-constructor/internal/core/entity/education"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewLevel(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		Name        string
+		Params      education.LevelParam
+		ShouldBeErr bool
+		ExpectedErr error
+	}{
+		{
+			Name: "without_error",
+			Params: education.LevelParam{
+				ID:    uuid.NewString(),
+				Title: "Test Program",
+			},
+			ShouldBeErr: false,
+			ExpectedErr: nil,
+		},
+		{
+			Name: "id_is_empty",
+			Params: education.LevelParam{
+				Title: "Test Program",
+			},
+			ShouldBeErr: true,
+			ExpectedErr: education.ErrLevelIDIsEmpty,
+		},
+		{
+			Name: "title_is_empty",
+			Params: education.LevelParam{
+				ID: uuid.NewString(),
+			},
+			ShouldBeErr: true,
+			ExpectedErr: education.ErrLevelTitleIsEmpty,
+		},
+	}
+
+	for i := range testCases {
+		c := testCases[i]
+
+		t.Run(t.Name(), func(t *testing.T) {
+			t.Parallel()
+
+			s, err := education.NewLevel(c.Params)
+
+			if c.ShouldBeErr {
+				t.Run("err_is", func(t *testing.T) {
+					require.ErrorIs(t, err, c.ExpectedErr)
+				})
+
+				return
+			}
+
+			t.Run("no_err", func(t *testing.T) {
+				require.NoError(t, err)
+
+				t.Run("code", func(t *testing.T) {
+					require.Equal(t, c.Params.ID, s.ID())
+				})
+
+				t.Run("title", func(t *testing.T) {
+					require.Equal(t, c.Params.Title, s.Title())
+				})
+
+			})
+		})
+	}
+}
